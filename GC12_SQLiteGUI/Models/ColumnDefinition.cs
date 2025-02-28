3d067d4087dab6e000110245dbbf3cc5c270d24e
GDcocos12 <124127742+GDcocos12@users.noreply.github.com>
2025-02-28 10:34:51 +0400

    Add files via upload

---
+++ GC12_SQLiteGUI/Models/ColumnDefinition.cs
@@ -0,0 +1,11 @@
+﻿namespace GC12_SQLiteGUI.Models
+{
+    public class ColumnDefinition
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public bool IsPrimaryKey { get; set; }
+        public bool IsNotNull { get; set; }
+
+    }
+}
